24c735e6cbedccefaa746126a6286425c021ec7e
Juraj Chlebec <havran@gmail.com>
2018-02-25 08:08:23 +0100

    remove duplicate create method

--- app/controllers/api/v1/books/crud_controller.rb
+++ app/controllers/api/v1/books/crud_controller.rb
@@ -19,23 +19,6 @@ module Api::V1
         }
       end
 
-      #  POST /book-add(.:format)          api/v1/books/crud#create
-      def create
-        book = Book.new
-        BookRepresenter.new(book).from_json(request.body.string)
-        book.save!
-
-        render status: 200, json: {
-          status: 200,
-          message: "Book saved!"
-        }
-      rescue ActiveRecord::RecordInvalid => e
-        render status: 422, json: {
-          status: 422,
-          message: "Invalid request! #{e.message}"
-        }
-      end
-
       # GET  /:id(.:format)               api/v1/books/crud#read
       def read
         book = Book.find_by(id: params[:id])
